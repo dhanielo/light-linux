63a3872e239a0233b29193fca3b7510b85ed3ca4
Daniel Oliveira <daniel@headdev.com.br>
2014-10-07 16:10:33 -0300

    fix controllers

--- MVVM/Application/docroot/js/controllers.js
+++ MVVM/Application/docroot/js/controllers.js
@@ -3,7 +3,7 @@
 var lightControllers = angular.module('lightControllers', []);
 
 lightControllers.controller('tasksController', ['$scope', '$http',
-	function ($scope, $http) {
+	function ($scope, $http, $location) {
 		$http.get('http://'+$location+'/getTaskMockup').success(function(data) {
 			$scope.data = data;
 		});
